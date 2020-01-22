d6a9f7b5dc48dcd2ac07ebffd7a79d8b98aeea57
ArthrVolfson <arthurvolfson@gmail.com>
2020-01-21 18:50:43 -0600

    done

--- context.js
+++ context.js
@@ -8,86 +8,93 @@
   Context is the value of the "this" keyword which is a reference to the object that "owns" the executing code
 */
 
-
 ////////// PROBLEM 1 //////////
 
 /*
   Create an object called user which has the following properties.
   username --> which is a string
   email --> which is a string
-  getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+  getUsername --> which is a function that returns the current object's username property.
+   *Don't use 'user' instead use the 'this' keyword*
 */
-  
 
 //Code Here
-
-
+let user = {
+  username: 'somethig123',
+  email: 'something@gmail.com',
+  getUsername: function() {
+    return this.username;
+  }
+};
 
 ////////// PROBLEM 2 //////////
 
 /*
- Below we have the class Animal.  The eat method is using the "this" keyword.  Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1.  You can pass anything you want in for name, species and food.
+ Below we have the class Animal.  The eat method is using the "this" keyword. 
+ Use the "new" keyword to assign context to "this", and save the instance to a variable named animal1.
+  You can pass anything you want in for name, species and food.
 */
 
 class Animal {
   constructor(name, species, food) {
-    this.name = name
-    this.species = species
-    this.food = food
+    this.name = name;
+    this.species = species;
+    this.food = food;
   }
 
   eat() {
-    return this.name + ' is a ' + this.species + ' and likes to eat ' + this.food;
+    return (
+      this.name + ' is a ' + this.species + ' and likes to eat ' + this.food
+    );
   }
 }
 
 //Code Here
-
-
-
+const animal1 = new Animal('Arthur', 'Bear', 'PIckle');
+console.log(animal1);
 ////////// PROBLEM 3 //////////
 
 /*
-  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object; and save the bound function to a variable named whoSaysHi.  
+  Use the bind method to assign context of the "this" keyword in the sayHi function to the user object;
+  and save the bound function to a variable named whoSaysHi.  
 */
 
 function sayHi(greeting) {
-  return this.name + ' says ' + greeting
+  return this.name + ' says ' + greeting;
 }
 
 let who = {
   name: 'Scuba Steve',
   age: 35,
   location: 'Belize'
-}
+};
 
 //Code Here
-
-
-
+const whoSaysHi = sayHi.bind(who);
 ////////// PROBLEM 4 //////////
 
 /*
-  here we have a function that just returns the "this" keyword.  We will give context to "this", and your job is to tell us what the context is.
+  here we have a function that just returns the "this" keyword.
+  We will give context to "this", and your job is to tell us what the context is.
 */
 
 function whatIsThis() {
-  return this
+  return this;
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = whatIsThis();
 
 let product = {
   name: 'snake plant',
   price: 45.32,
   description: 'Beautiful plant that can help filter the air inside your house.'
-}
+};
 
-let func = whatIsThis.bind(product)
+let func = whatIsThis.bind(product);
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product;
 
 let vacation = {
   location: 'Hawaii',
@@ -95,11 +102,10 @@ let vacation = {
   days: 7,
   nights: 6,
   whatIsThis: whatIsThis
-}
+};
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
-
+let context3 = vacation;
 
 class Family {
   constructor(numParents, numKids, numPets) {
@@ -109,11 +115,11 @@ class Family {
   }
 
   whatIsThis() {
-    return this
+    return this;
   }
 }
 
-let family1 = new Family(2, 4, 1)
+let family1 = new Family(2, 4, 1);
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1;
